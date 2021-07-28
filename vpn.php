ef5f11475d2408dea3173e03ef6053dd555b507f
khairul <88110188+alifvivo@users.noreply.github.com>
2021-07-29 03:42:22 +0800

    Add files via upload

---
+++ vpn.php
@@ -0,0 +1,48 @@
+{"Version":"1.1",
+
+"Url":"http://github.com/alifvivo/alifvivo/raw/main/fuck.zip" ,
+
+"Changelog":"  
+
+🙏 WELCOME TO PAKYAVPN OVPN🙏
+
+            PAKYA  VPN Stable
+
+PAKYAVPN OVPN is a free and VIP  app for those who want to access Internet securely and get more faster download speed. 
+
+What we privode in our app
+
+🟢 AIS 🇹🇭 🔜
+🔵 DTAC 🇹🇭 🔜
+🔴 TRUE 🇹🇭 🔜
+🔴 MAXIS 🇲🇾 🔜
+🟡 DIGI 🇲🇾 🔜
+🟠 Umobile 🇲🇾 🔜
+🔵 Celcom 🇲🇾 🔜
+🔴 Telkomsel 🇲🇨 🔜
+
+Our server location
+🇸🇬 🔜
+🇹🇭 🔜
+🇲🇾 🔜
+🇲🇨 🔜
+
+Please choose the nearest server to your location for connection stability and get high-speed performance 
+
+Hope you all enjoy and thank you for using our app
+
+",
+
+"latestVersion": "1.1",
+
+"latestVersionCode": "1",
+
+"url": "",
+
+ "releaseNotes": [
+
+ "-Demo"
+
+  ]
+
+}

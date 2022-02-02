7f49de3b27f2207db2f9e84c5e9de7ddb8463873
khairul <88110188+alifvivo@users.noreply.github.com>
2022-02-02 19:13:25 +0800

    Add files via upload

---
+++ vpn.php
@@ -0,0 +1,40 @@
+{"Version":"1.27",
+
+"Url":"https://github.com/alifvivo/alifvivo/raw/main/pakyavpn.zip" ,
+
+"Changelog":"  
+
+🙏 WELCOME TO PAKYAVPN OVPN🙏
+
+            PAKYA  VPN Stable
+
+PAKYAVPN OVPN is a free and VIP  app for those who want to access Internet securely and get more faster download speed. 
+
+What we privode in our app
+
+🔴 MAXIS 🇸🇬 🔜
+🇸🇬NO MULTI LOGIN🇸🇬
+🇸🇬NO TORREN🇸🇬
+
+Our server location
+🇸🇬 🔜
+
+Please choose the nearest server to your location for connection stability and get high-speed performance 
+
+Hope you all enjoy and thank you for using our app
+
+",
+
+"latestVersion": "1.27",
+
+"latestVersionCode": "1",
+
+"url": "",
+
+ "releaseNotes": [
+
+ "-Demo"
+
+  ]
+
+}

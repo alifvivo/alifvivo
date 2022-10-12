6e6cdc8f9832d3a27439f6b5d985ce3fdd33548c
khairul <88110188+alifvivo@users.noreply.github.com>
2022-10-12 21:49:52 +0800

    Update vpn.php

--- vpn.php
+++ vpn.php
@@ -1,4 +1,4 @@
-{"Version":"1.34",
+{"Version":"1.35",
 
 "Url":"https://github.com/alifvivo/alifvivo/raw/main/pakyavpn.zip" ,
 
@@ -31,7 +31,7 @@ Hope you all enjoy and thank you for using our app
 
 ",
 
-"latestVersion": "1.34",
+"latestVersion": "1.35",
 
 "latestVersionCode": "1",
 
